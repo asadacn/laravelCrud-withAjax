2cea1fc0f1ac1bd4e36eea4f280fd4f4b1ef40c5
Asad <asadacn@gmail.com>
2020-06-08 13:46:08 +0600

    FrontEnd added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,103 @@
         {{-- Bootstrap --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         {{-- Scripts --}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+<style>
+body{
+font-family: Nunito
+}
+</style>
     </head>
     <body>
-       
+       <div class="container p-4">
+           <h1 class="display-3 text-muted">Laravel Crud - Contact App</h1>
+           <div class="card">
+               <div class="card-body">
+            <div class="form-row mb-2">
+                <div class="col-auto mb-2"> <a class="btn  btn-success border" href="#" data-toggle="modal" data-target="#staticBackdrop" >ADD</a></div>
+                <div class="col-sm-8"><input class="form-control" type="text" name="search" id="search" placeholder="Search contact..."></div>
+            </div>
+            <div class="table-responsive">
+                <h4>Contact list</h4>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+        </div>
+           </div>
+       </div>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Contact Form</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form>
+                <div class="form-group">
+                  <label for="name">Name <span class="text-danger">*</span></label>
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Contact name">
+                </div>
+                <div class="form-group">
+                  <label for="contact">Contact No. <span class="text-danger">*</span></label>
+                  <input type="text" name="contact" class="form-control" id="contact" placeholder="Contact number">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Email address">
+                  </div>
+                  <div class="form-group">
+                    <label for="address">Address</label>
+                    <textarea  name="address" class="form-control" id="address" placeholder="Contact address"></textarea>
+                  </div>
+                  <div class="form-group">
+                    <button class="btn btn-primary w-100">Save Contact</button>
+                  </div>
+              </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
     </body>
 </html>
